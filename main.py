27524e981a46d0e70c4391a26dd4ca79f7933ce3
Ivan Dutchak <ivan.dutchak@leapfroggers.io>
2022-08-10 12:08:56 +0200

    Increase timeouts to simulate

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ queue_name = os.getenv("AZURE_STORAGE_QUEUE_NAME")
 queue_client = QueueClient.from_connection_string(connect_str, queue_name)
 
 while True:
-        message = queue_client.receive_message(visibility_timeout=10) # 20*60 = 20 minutes, 10 = 10 sec
+        message = queue_client.receive_message(visibility_timeout=6*60) # 20*60 = 20 minutes, 10 = 10 sec
         if message is None:
                 print("no messages")
                 time.sleep(10)
         else:
                 print("start " + message.content)
-                time.sleep(2)
+                time.sleep(5*60)
                 print("end " + message.content)
                 queue_client.delete_message(message)
                 time.sleep(1)
