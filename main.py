0a89af30398dab96cb5d8f4ba6114a10c55eb792
Ivan Dutchak <ivan.dutchak@leapfroggers.io>
2022-08-10 11:49:58 +0200

    Simplify service (remove background process)

--- main.py
+++ main.py
@@ -2,36 +2,21 @@
 
 from azure.storage.queue import QueueClient
 import os, time
-import _thread
-from flask import Flask
 
 print("start")
 
-def process():
-        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-        queue_name = os.getenv("AZURE_STORAGE_QUEUE_NAME")
-        queue_client = QueueClient.from_connection_string(connect_str, queue_name)
+connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+queue_name = os.getenv("AZURE_STORAGE_QUEUE_NAME")
+queue_client = QueueClient.from_connection_string(connect_str, queue_name)
 
-        while True:
-                message = queue_client.receive_message(visibility_timeout=10) # 20*60 = 20 minutes, 10 = 10 sec
-                if message is None:
-                        print("no messages")
-                        time.sleep(10)
-                else:
-                        print("start " + message.content)
-                        time.sleep(2)
-                        print("end " + message.content)
-                        queue_client.delete_message(message)
-                        time.sleep(1)
-
-_thread.start_new(process, ())
-
-app = Flask(__name__)
-
-@app.route("/health")
-def health():
-    return "200 OK", 200
-
-if __name__ == "__main__":
-    app.run(port=80)
-
+while True:
+        message = queue_client.receive_message(visibility_timeout=10) # 20*60 = 20 minutes, 10 = 10 sec
+        if message is None:
+                print("no messages")
+                time.sleep(10)
+        else:
+                print("start " + message.content)
+                time.sleep(2)
+                print("end " + message.content)
+                queue_client.delete_message(message)
+                time.sleep(1)
